fa6cff32d997f02c5af253efdfa00ec6ffb9f075
gtgopinath5 <gtgopinath5@gmail.com>
2024-11-04 14:49:58 +0530

    first commit

--- socket/socket.js
+++ socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "threadsapp-frontend.vercel.app",
+		origin: "https://threadsapp-frontend.vercel.app",
 		methods: ["GET", "POST"],
 	},
 });
